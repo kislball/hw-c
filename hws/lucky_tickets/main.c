7936f5fb3e901a0a83ef8dfb9b4aa9a214f4cdce
Georgiy Mikhailenko <kislball@yahoo.com>
2025-09-12 12:53:07 +0300

    Use int instead of char

--- hws/lucky_tickets/main.c
+++ hws/lucky_tickets/main.c
@@ -3,12 +3,12 @@
 int main() {
 	int n = 0;
 
-	for (char a = 0; a <= 9; a++)
-		for (char b = 0; b <= 9; b++)
-			for (char c = 0; c <= 9; c++)
-				for (char d = 0; d <= 9; d++)
-					for (char e = 0; e <= 9; e++)
-						for (char f = 0; f <= 9; f++) {
+	for (int a = 0; a <= 9; a++)
+		for (int b = 0; b <= 9; b++)
+			for (int c = 0; c <= 9; c++)
+				for (int d = 0; d <= 9; d++)
+					for (int e = 0; e <= 9; e++)
+						for (int f = 0; f <= 9; f++) {
 							if (a+b+c == d+e+f) {
 								n += 1;
 							}

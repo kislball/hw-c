d04daa8c63322406be02d155178cc83583826efa
Georgiy Mikhailenko <kislball@yahoo.com>
2025-09-12 12:49:49 +0300

    Add fast polynominal homework

---
+++ hws/fast_polynominal/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int calculate_polynominal(int x) {
+	int x2 = x*x;
+	return (x2+x)*(x2+1) + 1;
+}
+
+int main() {
+	int input = 0;
+	scanf("%d", &input);
+
+	int value = calculate_polynominal(input);
+
+	printf("x^4 + x^3 + x^2 + x + 1 = %d при x = %d\n", value, input);
+	return 0;
+}

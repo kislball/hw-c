e26f824ba7908a915ccfd235346fbf1b60243a63
Georgiy Mikhailenko <kislball@yahoo.com>
2025-09-15 22:17:46 +0300

    Add and implement null_elements homework

---
+++ hws/null_elements/main.c
@@ -0,0 +1,18 @@
+#include <stdhw.h>
+#include <stdio.h>
+
+int main(void) {
+	int len = 0;
+	int n = 0;
+	int* elements = PromptIntArray("Необходим массив", &len);
+
+	for (int i = 0; i < len; i++) {
+		if (elements[i] == 0) {
+			n += 1;
+		}
+	}
+
+	printf("Ответ: %d\n", n);
+
+	return 0;
+}

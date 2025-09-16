1f93b01631f55ac099533856abeef24dad77e71c
Georgiy Mikhailenko <kislball@yahoo.com>
2025-09-16 23:04:14 +0300

    Add parenthesis homework

---
+++ hws/paren/main.c
@@ -0,0 +1,33 @@
+#include <stdbool.h>
+#include <stdhw.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    char* input = PromptString("S=");
+    int n = 0;
+    bool ok = true;
+
+    int len = strlen(input);
+    for (int i = 0; i < len; i++) {
+        if (input[i] == '(') {
+            n += 1;
+        } else if (input[i] == ')') {
+            n -= 1;
+        }
+
+        if (n < 0) {
+            ok = false;
+            break;
+        }
+    }
+
+    if (ok && n == 0) {
+        printf("CORRECT\n");
+    } else {
+        printf("INCORRECT\n");
+    }
+
+    return 0;
+}

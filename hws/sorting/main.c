6d3bd232e2a367756b613872cc683fb2dd2d6daa
Georgiy Mikhailenko <kislball@yahoo.com>
2025-09-25 22:54:57 +0300

    Change input and output format in the sorting homework

--- hws/sorting/main.c
+++ hws/sorting/main.c
@@ -1,13 +1,69 @@
 #include "sort.h"
 #include <stdhw.h>
+#include <string.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+int* readNumbers(int* len)
+{
+	char* initialStr = promptString("");
+	char* str = trim(initialStr);
+	int inputLen = strlen(str);
+	int totalNumbers = 1;
+	
+	for (int i = 0; i < inputLen - 1; i++) {
+		if (str[i] == ' ' && str[i + 1] >= '0' && str[i + 1] <= '9') {
+			totalNumbers += 1;
+		}
+	}		
+
+	int* numbers = malloc(sizeof(int) * totalNumbers);
+	int curNumber = 0;
+	int curNumberIdx = 0;
+
+	for (int i = 0; i < inputLen; i++) {
+		if (str[i] == ' ' || str[i] == '\n') {
+			numbers[curNumberIdx] = curNumber;
+			curNumber = 0;
+			curNumberIdx++;
+		} else if ('0' <= str[i] && str[i] <= '9') {
+			curNumber *= 10;
+			curNumber += (int) str[i] - '0';
+		}
+	}
+
+	numbers[curNumberIdx] = curNumber;
+	*len = totalNumbers;
+	free(initialStr);
+	return numbers;
+}
 
 int main()
 {
-    int len = 0;
-    int* arr = promptIntArray("Введите числа", &len);
-    sort(arr, len);
+    int n = 0;
+    int* arr = readNumbers(&n);
+    int* arrInitial = malloc(100 * sizeof(int));
+    for (int i = 0; i < n; i++) {
+	arrInitial[i] = arr[i];
+    }
 
-    for (int i = 0; i < len; i++)
+    memset(arrInitial, 0, 100);
+    sort(arr, n);
+
+    for (int i = 0; i < n; i++)
         printf("%d\n", arr[i]);
+
+    int changedPositions = 0;
+
+    for (int i = 0; i < n; i++) {
+	if (arr[i] != arrInitial[i]) {
+		changedPositions++;
+	}
+    }
+
+    free(arr);
+    free(arrInitial);
+
+    return changedPositions;
 }

bcfa9073ce559843a8e2c1a6fcacf9ae19c7accb
Georgiy Mikhailenko <kislball@yahoo.com>
2025-09-25 13:48:18 +0300

    Implement sorting

---
+++ hws/sorting/main.c
@@ -0,0 +1,12 @@
+#include <stdhw.h>
+#include <stdio.h>
+#include "sort.h"
+
+int main()
+{
+	int len = 0;
+	int *arr = promptIntArray("Введите числа", &len);
+	sort(arr, len);
+
+	for (int i = 0; i < len; i++) printf("%d\n", arr[i]);
+}

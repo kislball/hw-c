bb850e9c0eb1719946453f150a26a8b504595340
Georgiy Mikhailenko <kislball@yahoo.com>
2025-10-07 23:20:28 +0300

    Formatting

--- hws/advanced_balance/main.c
+++ hws/advanced_balance/main.c
@@ -1,79 +1,80 @@
 #include "stack.h"
-#include <stdhw.h>
-#include <string.h>
 #include <stdbool.h>
+#include <stdhw.h>
 #include <stdio.h>
+#include <string.h>
 
 typedef enum {
-	NotAParen = -1, // что угодно, но не нижеперечисленные символы
-	RoundParen = 0, // ()
-	SquareParen = 1, // []
-	WaveyParen = 2, // {}
+    NotAParen = -1, // что угодно, но не нижеперечисленные символы
+    RoundParen = 0, // ()
+    SquareParen = 1, // []
+    WaveyParen = 2, // {}
 } ParenType;
 
 bool isOpeningParen(char ch)
 {
-	return ch == '(' || ch == '[' || ch == '{'; 
+    return ch == '(' || ch == '[' || ch == '{';
 }
 
 bool isClosingParen(char ch)
 {
-	return ch == ')' || ch == ']' || ch == '}'; 
+    return ch == ')' || ch == ']' || ch == '}';
 }
 
 bool isParen(char ch)
 {
-	return isClosingParen(ch) || isOpeningParen(ch);
+    return isClosingParen(ch) || isOpeningParen(ch);
 }
 
 ParenType resolveParenType(char ch)
 {
-	switch (ch)
-	{
-		case '(':
-		case ')':
-			return RoundParen;
-		case '[':
-		case ']':
-			return SquareParen;
-		case '{':
-		case '}':
-			return WaveyParen;
-		default:
-			return NotAParen;
-	}
+    switch (ch) {
+    case '(':
+    case ')':
+        return RoundParen;
+    case '[':
+    case ']':
+        return SquareParen;
+    case '{':
+    case '}':
+        return WaveyParen;
+    default:
+        return NotAParen;
+    }
 }
 
-bool checkParenBalance(char *input)
+bool checkParenBalance(char* input)
 {
-	size_t len = strlen(input);
-	Stack parens = stackNew();
+    size_t len = strlen(input);
+    Stack parens = stackNew();
 
-	for (int i = 0; i < len; i++) {
-		char current = input[i];
-		if (!isParen(current)) continue;
-		ParenType type = resolveParenType(current);
+    for (int i = 0; i < len; i++) {
+        char current = input[i];
+        if (!isParen(current))
+            continue;
+        ParenType type = resolveParenType(current);
 
-		if (isOpeningParen(current)) {
-			stackPush(&parens, (int) type);
-		} else if (isClosingParen(current)) {
-			bool has = false;
-			ParenType onTop = stackPop(&parens, &has);
-			if (!has || onTop != type) return false;
-		}
-	}
+        if (isOpeningParen(current)) {
+            stackPush(&parens, (int)type);
+        } else if (isClosingParen(current)) {
+            bool has = false;
+            ParenType onTop = stackPop(&parens, &has);
+            if (!has || onTop != type)
+                return false;
+        }
+    }
 
-	bool has = false;
-	stackPeek(&parens, &has);
-	stackDelete(&parens);
-	return !has;
+    bool has = false;
+    stackPeek(&parens, &has);
+    stackDelete(&parens);
+    return !has;
 }
 
 int main(void)
 {
-	char* input = promptString("s=");
-	bool ok = checkParenBalance(input);
-	
-	printf("%s\n", ok ? "YES" : "NO");
-	return 0;
+    char* input = promptString("s=");
+    bool ok = checkParenBalance(input);
+
+    printf("%s\n", ok ? "YES" : "NO");
+    return 0;
 }

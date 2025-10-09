358a17b38f7372894bea753b1f0040b55dcd8d0d
Егор <kislball@yahoo.com>
2025-10-09 18:31:24 +0300

    Баланс скобок (#5)
    * Implement stack
    
    * Add stack to exports
    
    * Implement paren balance homework
    
    * Add better clangd support
    
    * Formatting
    
    * Remove redundant typedef
    
    * Naming fixes + set stack->head to NULL after deleting
    
    * Naming fixes
    
    * Use curly instead of wavey

---
+++ stdhw/stack.c
@@ -0,0 +1,57 @@
+#include "stack.h"
+#include <stdbool.h>
+#include <stdlib.h>
+
+Stack stackNew()
+{
+    Stack st = { .head = NULL };
+    return st;
+}
+
+bool stackPush(Stack* stack, int value)
+{
+    StackNode* node = malloc(sizeof(StackNode));
+    if (node == NULL) {
+        return false;
+    }
+
+    node->value = value;
+    node->next = stack->head;
+
+    stack->head = node;
+    return true;
+}
+
+int stackPeek(Stack* stack, bool* isSuccessful)
+{
+    if (stack->head == NULL) {
+        *isSuccessful = false;
+        return 0;
+    } else {
+        *isSuccessful = true;
+        return stack->head->value;
+    }
+}
+
+int stackPop(Stack* stack, bool* isSuccessful)
+{
+    int val = stackPeek(stack, isSuccessful);
+    if (!*isSuccessful) {
+        return 0;
+    }
+
+    StackNode* head = stack->head;
+    stack->head = head->next;
+    free(head);
+
+    return val;
+}
+
+void stackDelete(Stack* stack)
+{
+    bool has = true;
+    while (has) {
+        stackPop(stack, &has);
+    }
+    stack->head = NULL;
+}
---
+++ stdhw/stack.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <stdbool.h>
+
+// Элемент на стеке.
+typedef struct StackNode {
+    int value;
+    struct StackNode* next;
+} StackNode;
+
+// Стек, поддерживающий операции push, pop, peek.
+typedef struct {
+    StackNode* head;
+} Stack;
+
+// Инициализирует новый экземпляр структуры стек.
+Stack stackNew();
+// Освобождает память, занимаемую стеком.
+void stackDelete(Stack* stack);
+// Добавляет элемент на вершину стека. Возвращает false в случае ошибки, true - в ином случае.
+bool stackPush(Stack* stack, int vlaue);
+// Возвращает элемент с вершины стека, а затем убирает его. По указателю во втором аргументе устанавливается значение
+// true если в стеке есть элемент на вершине, в ином случае false.
+int stackPop(Stack* stack, bool* isSuccessful);
+// Возвращает элемент с вершины стека. По указателю во втором аргументе устанавливается значение
+// true если в стеке есть элемент на вершине, в ином случае false.
+int stackPeek(Stack* stack, bool* isSuccessful);

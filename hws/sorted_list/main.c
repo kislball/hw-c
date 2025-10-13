2b5fa35ed240553945a7c61f34818e4c6d55c3f9
Georgiy Mikhailenko <kislball@yahoo.com>
2025-10-13 13:28:52 +0300

    Basic sorted list

---
+++ hws/sorted_list/main.c
@@ -0,0 +1,43 @@
+#include "list.h"
+#include <stdhw.h>
+#include <stdio.h>
+
+int main()
+{
+	LinkedList list = linkedListNew(); 
+	bool work = true;
+
+	while (work) {
+		int op = prompt("Введите номер операции(0 - выход, 1 - добавление элемента, 2 - удалить значение, 3 - печать списка): ");
+		if (op < 0 || op > 3) {
+			printf("Неизвестная операция\n");
+			continue;
+		}
+
+
+		switch (op) {
+			case 0: work = false; break;
+			case 1: {
+				int value = prompt("Введите элемент: ");
+				int index = 0;
+
+				bool ok = linkedListInsert(&list, index, value);
+				printf("%s\n", ok ? "Элемент встроен в список" : "Неправильный индекс");
+
+				break;
+			}
+			case 2: {
+				int len = linkedListCount(&list);
+				printf("Текущая длина списка: %d, ", len);
+				int index = prompt("введите номер элемента для удаления: ");
+				bool res = linkedListRemove(&list, index);
+				printf("%s\n", res ? "Элемент удалён!" : "Неправильный индекс");
+				break;
+			}
+			case 3: linkedListPrintStdout(&list); printf("\n"); break;
+		}
+	}
+
+	linkedListDelete(&list);
+	return 0;
+}

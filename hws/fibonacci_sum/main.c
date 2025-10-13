2aec55b729154edc240aeb4f46fecfd4a4c15868
Georgiy Mikhailenko <kislball@yahoo.com>
2025-10-13 14:10:27 +0300

    Formatting

--- hws/fibonacci_sum/main.c
+++ hws/fibonacci_sum/main.c
@@ -6,18 +6,20 @@
 
 int main()
 {
-	int* fib = calloc(MAX_FIBONACCI, sizeof(int));
-	fib[0] = 1;
-	fib[1] = 1;
+    int* fib = calloc(MAX_FIBONACCI, sizeof(int));
+    fib[0] = 1;
+    fib[1] = 1;
 
-	for (int i = 2; i < MAX_FIBONACCI; i++) fib[i] = fib[i - 1] + fib[i - 2];
+    for (int i = 2; i < MAX_FIBONACCI; i++)
+        fib[i] = fib[i - 1] + fib[i - 2];
 
-	int sum = 0;
-	for (int i = 0; i < MAX_FIBONACCI; i += 1) {
-		if (fib[i] <= 1000000 && fib[i] % 2 == 0) sum += fib[i];
-	}
+    int sum = 0;
+    for (int i = 0; i < MAX_FIBONACCI; i += 1) {
+        if (fib[i] <= 1000000 && fib[i] % 2 == 0)
+            sum += fib[i];
+    }
 
-	printf("Ответ: %d\n", sum);
-	free(fib);
-	return 0;
+    printf("Ответ: %d\n", sum);
+    free(fib);
+    return 0;
 }
--- hws/palindrome/main.c
+++ hws/palindrome/main.c
@@ -1,30 +1,33 @@
 #include <ctype.h>
 #include <stdhw.h>
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 bool isPalindrome(char* input)
 {
-	int len = strlen(input);
-	char* left = input;
-	char* right = input + len - 1;
+    int len = strlen(input);
+    char* left = input;
+    char* right = input + len - 1;
 
-	while (left <= right) {
-		while (isspace(*left)) left++;
-		while (isspace(*right)) right--;
-		if (*left != *right) return false;
-		left++;
-		right--;
-	}
-	return true;
+    while (left <= right) {
+        while (isspace(*left))
+            left++;
+        while (isspace(*right))
+            right--;
+        if (*left != *right)
+            return false;
+        left++;
+        right--;
+    }
+    return true;
 }
 
 int main()
 {
-	char* input = promptString("Введите строку: ");
+    char* input = promptString("Введите строку: ");
 
-	printf("%s\n", isPalindrome(input) ? "YES" : "NO");
-	free(input);
-	return 0;
+    printf("%s\n", isPalindrome(input) ? "YES" : "NO");
+    free(input);
+    return 0;
 }

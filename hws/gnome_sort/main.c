5a9cb93a081a1224214f739988fab4e432ee23bc
Georgiy Mikhailenko <kislball@yahoo.com>
2025-10-13 13:51:01 +0300

    Implement gnome sort

---
+++ hws/gnome_sort/main.c
@@ -0,0 +1,35 @@
+#include <stdhw.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void gnomeSort(int* input, int len)
+{
+	int i = 1;
+	while (i < len) {
+		if (i == 0) i++;
+		if (input[i - 1] > input[i]) {
+			int temp = input[i - 1];
+			input[i - 1] = input[i];
+			input[i] = temp;
+			i--;
+		} else {
+			i++;
+		}
+	} 
+}
+
+int main()
+{
+	int len = 0;
+	int* input = promptIntArray("Введите массив чисел для сортировки: ", &len);
+
+	gnomeSort(input, len);
+
+	for (int j = 0; j < len; j++) {
+		printf("%d ", input[j]);
+	}
+	printf("\n");
+
+	free(input);
+	return 0;
+}

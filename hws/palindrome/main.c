8053145ddede370672d0f60514753bea5131213a
Georgiy Mikhailenko <kislball@yahoo.com>
2025-10-13 13:41:22 +0300

    Palindrome task

---
+++ hws/palindrome/main.c
@@ -0,0 +1,30 @@
+#include <ctype.h>
+#include <stdhw.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+bool isPalindrome(char* input)
+{
+	int len = strlen(input);
+	char* left = input;
+	char* right = input + len - 1;
+
+	while (left <= right) {
+		while (isspace(*left)) left++;
+		while (isspace(*right)) right--;
+		if (*left != *right) return false;
+		left++;
+		right--;
+	}
+	return true;
+}
+
+int main()
+{
+	char* input = promptString("Введите строку: ");
+
+	printf("%s\n", isPalindrome(input) ? "YES" : "NO");
+	free(input);
+	return 0;
+}

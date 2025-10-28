327e8448e505ee7ebca3fdfb9fd5c02eea58dd85
Егор <kislball@yahoo.com>
2025-10-28 14:30:44 +0300

    Контрольная работа 1 (#7)
    * Palindrome task
    
    * Implement gnome sort
    
    * Fix input prompt for gnome sort
    
    * Implement fibonacci sum homework
    
    * Formatting
    
    * Add documentation
    
    * More optimal solution of the Fibonacci sum problem without additional memory
    
    * More optimal solution of the Fibonacci sum problem without additional memory

---
+++ hws/palindrome/main.c
@@ -0,0 +1,36 @@
+#include <ctype.h>
+#include <stdhw.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Проверяет явяляется ли переданная строка палиндромом.
+// Игнорирует пробелы. Возвращает true или false.
+bool isPalindrome(char* input)
+{
+    int len = strlen(input);
+    char* left = input;
+    char* right = input + len - 1;
+
+    while (left <= right) {
+        while (isspace(*left))
+            left++;
+        while (isspace(*right))
+            right--;
+        if (*left != *right)
+            return false;
+        left++;
+        right--;
+    }
+
+    return true;
+}
+
+int main()
+{
+    char* input = promptString("Введите строку: ");
+
+    printf("%s\n", isPalindrome(input) ? "YES" : "NO");
+    free(input);
+    return 0;
+}

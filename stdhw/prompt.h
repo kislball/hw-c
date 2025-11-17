e0ba2b62cff923152c2cbac95a8b60a0dca51e7d
Georgiy Mikhailenko <kislball@yahoo.com>
2025-11-17 12:49:01 +0300

    Disable some lints due to being overly strict

--- stdhw/prompt.h
+++ stdhw/prompt.h
@@ -1,3 +1,8 @@
+// clang-tidy по какой-то причине жалуется на двойное объявление, но при этом
+// указывает на одно и то же место
+// см https://github.com/kislball/hw-c/actions/runs/19425092224/job/55570610971?pr=12/
+// NOLINTBEGIN
 int prompt(char* data);
 int* promptIntArray(char* prompt, int* len);
 char* promptString(char* prompt);
+// NOLINTEND

a3ae6cd1984d1764711c4b33c26dc711528dd4f1
Georgiy Mikhailenko <kislball@yahoo.com>
2026-02-18 00:05:45 +0300

    Add documentation for new stdhw routines

--- stdhw/strutil.h
+++ stdhw/strutil.h
@@ -2,5 +2,9 @@
 
 #include <stddef.h>
 
+// Добавляет к source символы character слева(с начала), чтобы длина была не менее targetSize.
+// Возвращаемая строка *всегда* выделяется в памяти за очистку этой памяти отвечает вызывающая функция.
 char* padLeft(char* source, size_t targetSize, char character);
+// Добавляет к source символы character справа(с конца), чтобы длина была не менее targetSize.
+// Возвращаемая строка *всегда* выделяется в памяти за очистку этой памяти отвечает вызывающая функция.
 char* padRight(char* source, size_t targetSize, char character);

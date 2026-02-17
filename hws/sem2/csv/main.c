5bb4bae9fb778b2952f298e3afc52df60c65835d
Georgiy Mikhailenko <kislball@yahoo.com>
2026-02-18 00:00:46 +0300

    Add output to file

--- hws/sem2/csv/main.c
+++ hws/sem2/csv/main.c
@@ -1,26 +1,25 @@
 #include "common.h"
 #include "csv.h"
 #include "display.h"
+#include <assert.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-void usage(char* name)
-{
-    printf("%s usage: %s <filename>\n", name, name);
-}
-
 int main(int argc, char** argv)
 {
-    if (argc != 2) {
-        fprintf(stderr, "%s: expected 2 arguments, got - %d\n", argv[0], argc);
-        usage(argv[0]);
-        return 1;
-    }
-    FILE* fptr = fopen(argv[1], "r");
+    char* inputName = "input.csv";
+    char* outputName = "output.txt";
+    if (argc >= 2)
+        inputName = argv[1];
+    if (argc >= 3)
+        outputName = argv[2];
+    assert(inputName != NULL && outputName != NULL);
+
+    FILE* fptr = fopen(inputName, "r");
     if (fptr == NULL) {
-        fprintf(stderr, "%s: failed to open %s: %s\n", argv[0], argv[1], strerror(errno));
+        fprintf(stderr, "%s: failed to open %s: %s\n", argv[0], inputName, strerror(errno));
         return 1;
     }
     CSVTable* table = csvNew();
@@ -30,7 +29,16 @@ int main(int argc, char** argv)
         csvGetCellsView(table),
         csvWidth(table),
         csvHeight(table));
-    printf("%s", result);
+
+    FILE* out = fopen(outputName, "w");
+    if (out == NULL) {
+        fprintf(stderr, "%s: failed to open %s: %s\n", argv[0], outputName, strerror(errno));
+        free(result);
+        csvFree(&table);
+        return 1;
+    }
+    fprintf(out, "%s", result);
+    fclose(out);
 
     free(result);
     csvFree(&table);

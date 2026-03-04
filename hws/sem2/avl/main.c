b4c3c0b17010586b29b9c68d612e8462d3bf3932
Georgiy Mikhailenko <kislball@yahoo.com>
2026-03-04 20:27:51 +0300

    Format

--- hws/sem2/avl/main.c
+++ hws/sem2/avl/main.c
@@ -7,59 +7,66 @@
 static char* fileName = nullptr;
 static Map* data = nullptr;
 
-void writeDb() {
+void writeDb()
+{
     FILE* f = fopen(fileName, "w");
-    if (!f)
-        { fprintf(stderr, "Failed to write DB: %s\n", strerror(errno)); exit(1); }
+    if (!f) {
+        fprintf(stderr, "Failed to write DB: %s\n", strerror(errno));
+        exit(1);
+    }
     mapExport(data, f);
     fclose(f);
 }
 
-bool runCommand(char* cmd) {
-	char commandName[16] = {};
-	char argument[512] = {};
-	sscanf(cmd, "%15[^ \n] %511[^\n]", commandName, argument);
-	printf("\"%s\" \"%s\"\n", commandName, argument);
+bool runCommand(char* cmd)
+{
+    char commandName[16] = { };
+    char argument[512] = { };
+    sscanf(cmd, "%15[^ \n] %511[^\n]", commandName, argument);
+    printf("\"%s\" \"%s\"\n", commandName, argument);
 
-	if (!strcmp(commandName, "quit")) return false;
-	else if (!strcmp(commandName, "save")) {
-		writeDb();
-		return true;
-	}
-	else if (!strcmp(commandName, "delete")) {
-		mapDelete(&data, argument);
-	} else if (!strcmp(commandName, "find")) {
-		char* value = nullptr;
-		bool ok = mapGet(data, argument, &value);
-		if (ok) {
-			printf("%s -> %s\n", argument, value);
-			free(value);
-		} else {
-			fprintf(stderr, "Airport \"%s\" not found\n", argument);
-		}
-		return true;
-	} else if (!strcmp(commandName, "add")) {
-		char code[16] = {};
-		char name[128] = {};
-		sscanf(argument, "%15[^:]:%127[^\n]", code, name);
-		mapInsert(&data, code, name);
-		printf("Added record %s -> %s\n", code, name);
-		return true;
-	} else {
-		fprintf(stderr, "Unknown command\n");
-		return true;
-	}
+    if (!strcmp(commandName, "quit"))
+        return false;
+    else if (!strcmp(commandName, "save")) {
+        writeDb();
+        return true;
+    } else if (!strcmp(commandName, "delete")) {
+        mapDelete(&data, argument);
+    } else if (!strcmp(commandName, "find")) {
+        char* value = nullptr;
+        bool ok = mapGet(data, argument, &value);
+        if (ok) {
+            printf("%s -> %s\n", argument, value);
+            free(value);
+        } else {
+            fprintf(stderr, "Airport \"%s\" not found\n", argument);
+        }
+        return true;
+    } else if (!strcmp(commandName, "add")) {
+        char code[16] = { };
+        char name[128] = { };
+        sscanf(argument, "%15[^:]:%127[^\n]", code, name);
+        mapInsert(&data, code, name);
+        printf("Added record %s -> %s\n", code, name);
+        return true;
+    } else {
+        fprintf(stderr, "Unknown command\n");
+        return true;
+    }
 
-	return true; // to avoid compiler warnings
+    return true; // to avoid compiler warnings
 }
 
 void readDb()
 {
     FILE* f = fopen(fileName, "r");
-    if (!f)
-        { fprintf(stderr, "Failed to open DB: %s\n", strerror(errno)); exit(1); }
+    if (!f) {
+        fprintf(stderr, "Failed to open DB: %s\n", strerror(errno));
+        exit(1);
+    }
 
-    if (data) mapFree(&data);
+    if (data)
+        mapFree(&data);
     data = mapImport(f);
     fclose(f);
 }
@@ -79,7 +86,8 @@ int main(int argc, char** argv)
     while (true) {
         printf(">: ");
         fgets(buffer, 512, stdin);
-	if (!runCommand(buffer)) break;
+        if (!runCommand(buffer))
+            break;
     }
 
     mapFree(&data);

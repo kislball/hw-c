d8f4d4b4be505df33dd669b04cb41c3a0623a1b2
Georgiy Mikhailenko <kislball@yahoo.com>
2026-03-16 11:52:04 +0300

    Fix lint error

--- hws/sem2/states/main.c
+++ hws/sem2/states/main.c
@@ -1,11 +1,17 @@
 #include "alg.h"
 #include "data.h"
+#include <errno.h>
+#include <string.h>
 
 int main(int argc, char** argv)
 {
     if (argc != 2)
         return 1;
     FILE* f = fopen(argv[1], "r");
+    if (f == NULL) {
+        printf("%s: failed to open file: %s\n", argv[0], strerror(errno));
+        return 1;
+    }
     Input* input = inputRead(f);
     Output* solution = solve(input);
 
